6c360cd565497c3d95f2ca7be2795e2a8f5d7448
Alex Dolid <dolid.sasha@gmail.com>
2024-11-30 21:45:35 +0200

    Create Kerberos.test.js

---
+++ test/Kerberos.test.js
@@ -0,0 +1,80 @@
+import { describe, it } from 'node:test';
+import { strict as assert } from 'node:assert';
+
+import { commonRolesPolicy, principalsPolicy, resourcesPolicy, expensePolicy } from './mocks/index.js';
+
+import { Effect, Kerberos } from '../src/index.js';
+
+describe('Kerberos', () => {
+  const kerberos = new Kerberos([expensePolicy], [commonRolesPolicy]);
+
+  describe('isAllowed', () => {
+    it('should return true if the action is allowed', () => {
+      const principal = principalsPolicy.sally;
+      const resource = resourcesPolicy.expense1;
+      const action = 'view';
+
+      const isAllowed = kerberos.isAllowed({ principal, action, resource });
+
+      assert.strictEqual(isAllowed, true);
+    });
+
+    it('should return false if the action is not allowed', () => {
+      const principal = principalsPolicy.sally;
+      const resource = resourcesPolicy.expense1;
+      const action = 'approve';
+
+      const isAllowed = kerberos.isAllowed({ principal, action, resource });
+
+      assert.strictEqual(isAllowed, false);
+    });
+  });
+
+  describe('checkResources', () => {
+    it('should return the effect actions map for each resource (Effect mode)', () => {
+      const principal = principalsPolicy.sally;
+      const resources = [
+        { resource: resourcesPolicy.expense1, actions: ['view', 'create', 'delete'] },
+        { resource: resourcesPolicy.expense4, actions: ['view', 'create'] },
+      ];
+
+      const results = kerberos.checkResources({ principal, resources });
+
+      assert.deepStrictEqual(results, {
+        results: [
+          {
+            resource: { id: 'expense1', kind: 'expense' },
+            actions: { view: Effect.Allow, create: Effect.Allow, delete: Effect.Deny },
+          },
+          {
+            resource: { id: 'expense4', kind: 'expense' },
+            actions: { view: Effect.Deny, create: Effect.Allow },
+          },
+        ],
+      });
+    });
+
+    it('should return the effect actions map for each resource (Boolean mode)', () => {
+      const principal = principalsPolicy.sally;
+      const resources = [
+        { resource: resourcesPolicy.expense1, actions: ['view', 'create', 'delete'] },
+        { resource: resourcesPolicy.expense4, actions: ['view', 'create'] },
+      ];
+
+      const results = kerberos.checkResources({ principal, resources }, true);
+
+      assert.deepStrictEqual(results, {
+        results: [
+          {
+            resource: { id: 'expense1', kind: 'expense' },
+            actions: { view: true, create: true, delete: false },
+          },
+          {
+            resource: { id: 'expense4', kind: 'expense' },
+            actions: { view: false, create: true },
+          },
+        ],
+      });
+    });
+  });
+});
